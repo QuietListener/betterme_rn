596e0b272fde5f1ce6b97a918aeacc1d5c72dd65
junjun <junjun@baicizhan.com>
2018-04-13 15:58:35 +0800

    download ok

--- betterme/videoList.js
+++ betterme/videoList.js
@@ -29,9 +29,8 @@ const name = "KasivaMutua_2017G-950k.mp4";
 const downloadDest = `${downloadDir}/${name}`;
 const srtDest = `${downloadDir}/KasivaMutua_2017G-950k.srt`;
 
-const DownloadIng = "0";
-const DownloadDone = "1";
-const DownloadError = "2";
+
+const DownloadError = -100;
 
 import Realm from "realm"
 import {DownloadItem} from "./db/models"
@@ -159,16 +158,16 @@ class VideoList extends Component
       },
       progress: (res) => {
 
-        let pro = Math.round(res.bytesWritten*100.0 / res.contentLength);
+        let pro = (res.bytesWritten*100.0 / res.contentLength).toFixed(1);
         console.log("progress:",res,pro)
 
-        if(pro%5 == 0)
+        if(pro%5 < 0.1)
         {
-          this.createOrUpdate(DownloadItem,{id:key,progress:pro})
+          that.createOrUpdate(DownloadItem,{id:key,progress:pro})
         }
         var params = {}
         params[key] = pro;
-        this.setState(params);
+        that.setState(params);
       }
     };
     try {
@@ -182,12 +181,21 @@ class VideoList extends Component
         this.createOrUpdate(DownloadItem,{id:key,progress:100})
       }).catch(err => {
         console.log('err', err);
-        //alert(err)
+
+        that.createOrUpdate(DownloadItem,{id:key,progress:DownloadError})
+        var params = {}
+        params[key] = DownloadError;
+        that.setState(params);
       });
     }
     catch (e) {
       console.error(e);
       //alert(e)
+
+      that.createOrUpdate(DownloadItem,{id:key,progress:DownloadError})
+      var params = {}
+      params[key] = DownloadError;
+      that.setState(params);
     }
 
   }
@@ -233,6 +241,20 @@ class VideoList extends Component
        progress_video = this.state.orgin_download_state[key_video];
      }
 
+     var progress_video_str = null;
+     if(progress_video)
+     {
+          if(progress_video>=0)
+          {
+            progress_video_str = `${progress_video}%`;
+          }
+          else if(progress_video == DownloadError)
+          {
+            progress_video_str = "下载失败请重试"
+          }
+     }
+
+
       var progress_srt = null;
       if(this.state[key_video])
       {
@@ -243,19 +265,31 @@ class VideoList extends Component
         progress_srt = this.state.orgin_download_state[key_srt];
       }
 
+      var progress_srt_str = null;
+      if(progress_srt)
+      {
+        if(progress_srt>=0)
+        {
+          progress_srt_str = `${progress_srt}%`;
+        }
+        else if(progress_srt == DownloadError)
+        {
+          progress_srt_str = "下载失败请重试"
+        }
+      }
 
 
      return <View style={{height:200,width:base.ScreenWidth,flexDirection:"row",padding:4,margin:4,marginBottom:8,borderWidth:1,}}>
                <View style={{width:150+2,height:190}}>
-                 <Image style={{width:150,height:190,}} source={{uri:item.poster}} />
+                 {/*<Image style={{width:150,height:190,}} source={{uri:item.poster}} />*/}
                </View>
 
                <View style={{width:base.ScreenWidth-180}}>
                  <Text>{item.title}</Text>
 
-                 <Text style={{margin:10}} onPress={()=>this.download(item.videoUrl,video_path, key_video)}>下载视频 {progress_video?`${progress_video}%`:null}</Text>
+                 <Text style={{margin:10}} onPress={()=>this.download(item.videoUrl,video_path, key_video)}>下载视频 {progress_video_str}</Text>
 
-                 <Text  style={{margin:10}} onPress={()=>this.download(item.srtUrl,srt_path,key_srt)} >下载字幕  {progress_srt?`${progress_srt}%`:null}</Text>
+                 <Text  style={{margin:10}} onPress={()=>this.download(item.srtUrl,srt_path,key_srt)} >下载字幕  {progress_srt_str}</Text>
 
                  <Text style={{marginTop:20}} onPress={()=>{
                    this.props.navigation.navigate("Video",{videoPath:video_path,srtPath:srt_path})
